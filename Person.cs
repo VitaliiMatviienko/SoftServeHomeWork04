46da4f25322a142ed45e1cd2481c2f8ad4625b8f
VitaliiMatviienko <vitalijmatvienko@gmail.com>
2023-04-05 15:17:02 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftServeHomeWork04
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public string Name {
+            get { return name; }
+        }
+
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+        }
+
+        public Person() { name = "Unknown"; birthYear = new DateTime(1900, 1, 1); }
+        public Person(string name, DateTime birthYear) { this.name = name; this.birthYear = birthYear; }
+
+        public int Age()
+        {
+            return (int)(DateTime.Now.Subtract(birthYear).Days/365.25);
+        }
+        public static Person Input()
+        {
+            Console.Write("write name: ");
+            string name = Console.ReadLine();
+            Console.Write("write bithYear: ");
+            int birthYear = int.Parse(Console.ReadLine());
+            Person person = new Person(name, (new DateTime(birthYear, 1, 1)));
+            return person;
+        }
+
+        public void ChangeName(string newName)
+        {
+            //Console.Write("write new name: ");
+
+            //name = Console.ReadLine();
+            name = newName;
+
+
+        }
+
+        public override string ToString()
+        {
+            return "name - " + name + ", birthyear - " + birthYear.Year.ToString();
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first == second);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SoftServeHomeWork04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPersons = 6;
+            Person[] persons = new Person[countPersons];
+            for (int i = 0; i < countPersons; i++)
+            {
+                persons[i] = Person.Input();
+            }
+
+
+            for (int k = 0; k < countPersons; k++)
+            {
+                Console.WriteLine("person: {0}. age: {1}", persons[k].ToString(), persons[k].Age());
+            }
+
+            for (int l = 0; l < countPersons; l++)
+            {
+                if (persons[l].Age() < 16)
+                {
+                    persons[l].ChangeName("Very young");
+                }
+                
+            }
+            for (int j = 0; j < countPersons; j++)
+            {
+
+                persons[j].Output();
+            }
+
+            for (int m = 0; m < persons.Length; m++)
+            {
+                for (int n = m + 1; n < persons.Length; n++)
+                {
+                    if (persons[m] == persons[n])
+                    {
+                        Console.WriteLine("{0} and {1} are equals", persons[m].ToString(), persons[n].ToString());
+                    }
+                    else
+                    { Console.WriteLine($"Cars are not equals"); }
+                }
+            }
+
+        }
+    }
+}
